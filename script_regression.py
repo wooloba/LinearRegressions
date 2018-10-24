736902870d72000fafae112b947a396345b6e38a
YAOZHI LU <luyaozhiusing@gmail.com>
2018-10-23 23:20:14 -0600

    added lasso

---
+++ script_regression.py
@@ -0,0 +1,92 @@
+from __future__ import division  # floating point division
+import csv
+import random
+import math
+import numpy as np
+
+import dataloader as dtl
+import regressionalgorithms as algs
+
+def l2err(prediction,ytest):
+    """ l2 error (i.e., root-mean-squared-error) """
+    return np.linalg.norm(np.subtract(prediction,ytest))
+
+def l1err(prediction,ytest):
+    """ l1 error """
+    return np.linalg.norm(np.subtract(prediction,ytest),ord=1)
+
+def l2err_squared(prediction,ytest):
+    """ l2 error squared """
+    return np.square(np.linalg.norm(np.subtract(prediction,ytest)))
+
+def geterror(predictions, ytest):
+    # Can change this to other error values
+    return l2err(predictions,ytest)/ytest.shape[0]
+
+
+if __name__ == '__main__':
+    trainsize = 1000
+    testsize = 5000
+    numruns = 5
+
+    regressionalgs = {#'Random': algs.Regressor(),
+                #'Mean': algs.MeanPredictor(),
+                #'FSLinearRegression5': algs.FSLinearRegression({'features': [e+1 for e in range(384)]}),
+                #'FSLinearRegression50': algs.FSLinearRegression({'features': range(50)}),
+                #'RidgeLinearRegression': algs.RidgeLinearRegression(lam=0.01,parameters={'features': range(384)}),
+                'LassoLinearRegression': algs.LassoLinearRegression(parameters={'features':range(384)})
+             }
+    numalgs = len(regressionalgs)
+
+    # Enable the best parameter to be selected, to enable comparison
+    # between algorithms with their best parameter settings
+    parameters = (
+        {'regwgt': 0.0},
+        {'regwgt': 0.01},
+        {'regwgt': 1.0},
+                      )
+    numparams = len(parameters)
+    
+    errors = {}
+    for learnername in regressionalgs:
+        errors[learnername] = np.zeros((numparams,numruns))
+
+    for r in range(numruns):
+        print("this is "+str(r)+" run.")
+        trainset, testset = dtl.load_ctscan(trainsize,testsize)
+        print(('Running on train={0} and test={1} samples for run {2}').format(trainset[0].shape[0], testset[0].shape[0],r))
+
+        for p in range(numparams):
+            params = parameters[p]
+            for learnername, learner in regressionalgs.items():
+                # Reset learner for new parameters
+                learner.reset(params)
+                print ('Running learner = ' + learnername + ' on parameters ' + str(learner.getparams()))
+                # Train model
+                learner.learn(trainset[0], trainset[1])
+                # Test model
+                predictions = learner.predict(testset[0])
+                error = geterror(testset[1], predictions)
+
+                print ('Error for ' + learnername + ': ' + str(error))
+                errors[learnername][p,r] = error
+        #print(errors)
+
+    for learnername in regressionalgs:
+        besterror = np.mean(errors[learnername][0,:])
+        bestparams = 0
+        for p in range(numparams):
+            aveerror = np.mean(errors[learnername][p,:])
+
+            sdError = np.std(errors[learnername][p,:])/np.sqrt(numruns)
+            #print(errors[learnername][p,:],numruns)
+
+            if aveerror < besterror:
+                besterror = aveerror
+                bestparams = p
+
+        # Extract best parameters
+        learner.reset(parameters[bestparams])
+        #print ('Best parameters for ' + learnername + ': ' + str(learner.getparams()))
+        print ('Average error for ' + learnername + ': ' + str(besterror))
+        print ('Standard error for ' + learnername + ': ' + str(sdError))
